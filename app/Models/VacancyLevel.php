701300fa2ea9ec5be26ec71ea08ad320d548b6ba
K-taiga <taigakatou0329@gmail.com>
2021-05-10 08:20:18 +0900

    add slugTest

--- app/Models/VacancyLevel.php
+++ app/Models/VacancyLevel.php
@@ -14,19 +14,28 @@ class VacancyLevel extends Model
 
     public function mark(): string
     {
-        if ($this->remainingCount === 0) {
-            return '×';
-        }
-        if ($this->remainingCount < 5) {
-            return '△';
-        }
-        return '◎';
+        $marks = ['empty' => '×', 'few' => '△', 'enough' => '◎'];
+        $slug = $this->slug();
+        assert(isset($marks[$slug]), new \DomainException('invalid slug value'));
+
+        return $marks[$slug];
     }
 
     // マジックメソッド　文字列に変換される際は以下のメソッドを実行
     public function __toString()
     {
         return $this->mark();
+    }
+
+    public function slug(): string
+    {
+        if ($this->remainingCount === 0) {
+            return 'empty';
+        }
+        if ($this->remainingCount < 5) {
+            return 'few';
+        }
+        return 'enough';
     }
 
 }
--- tests/Unit/Models/VacancyLevelTest.php
+++ tests/Unit/Models/VacancyLevelTest.php
@@ -37,4 +37,35 @@ class VacancyLevelTest extends TestCase
             ],
         ];
     }
+
+    /**
+     * @param int $remainingCount
+     * @param string $expectedSlug
+     * @dataProvider dataSlug
+     */
+    public function testSlug(int $remainingCount, string $expectedSlug)
+    {
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedSlug, $level->slug());
+    }
+
+    public function dataSlug()
+    {
+        return [
+            // テストケース
+            '空きなし' => [
+                // 渡す変数
+                'remainingCount' => 0,
+                'expectedSlug' => 'empty',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedSlug' => 'few',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedSlug' => 'enough',
+            ],
+        ];
+    }
 }
