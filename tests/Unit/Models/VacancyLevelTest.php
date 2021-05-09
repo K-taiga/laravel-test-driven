dfc94283a1a9ae0d5d5f0016058a3d17adab6da3
K-taiga <taigakatou0329@gmail.com>
2021-05-09 17:15:51 +0900

    add VacancyLevelTest

---
+++ tests/Unit/Models/VacancyLevelTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\VacancyLevel;
+
+class VacancyLevelTest extends TestCase
+{
+    /**
+     * @param int $remainingCount
+     * @param string $expectedMark
+     * @dataProvider dataMark
+     */
+    public function testMarK(int $remainingCount, string $expectedMark)
+    {
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedMark, $level->mark());
+    }
+
+    public function dataMark()
+    {
+        return [
+            // テストケース
+            '空きなし' => [
+                // 渡す変数
+                'remainingCount' => 0,
+                'expectedMark' => '×',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedMark' => '△',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedMark' => '◎',
+            ],
+        ];
+    }
+}
